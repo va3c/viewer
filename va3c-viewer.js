0c6b05b507bdca9fb9abcf4c7f10e5a628a838b0
Theo Armour <t.armour@gmail.com>
2014-05-17 21:41:23 -0400

    lights

--- va3c-viewer.js
+++ va3c-viewer.js
@@ -4,6 +4,8 @@
 	var obj;
 	VA3C.fname = '../json/test.js';
 
+	var pi = Math.PI, pi05 = pi * 0.5, pi2 = pi + pi;
+	var d2r = pi / 180, r2d = 180 / pi;  // degrees / radians
 
 
 	function init() {
@@ -24,18 +26,40 @@
 
 		VA3C.renderer = new THREE.WebGLRenderer( { alpha: 1, antialias: true, clearColor: 0xffffff }  );
 		VA3C.renderer.setSize( window.innerWidth, window.innerHeight );
+		VA3C.renderer.shadowMapEnabled = true;
 		document.body.appendChild( VA3C.renderer.domElement );
 		VA3C.scene = new THREE.Scene();
 
-		VA3C.camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+		VA3C.camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
 		VA3C.camera.position.set( 10, 10, 10 );
 		VA3C.controls = new THREE.TrackballControls( VA3C.camera, VA3C.renderer.domElement );
 
 		VA3C.scene.add(new THREE.AmbientLight(0x444444));
 
 // light
+/*
 		var light = new THREE.PointLight( 0xffffff, 1 );
 		light.position = VA3C.camera.position;
+		VA3C.scene.add( light );
+*/
+
+ 		light = new THREE.DirectionalLight( 0xffffff, 1 );
+
+		var pos = convertPosition(  -23, -46, 500 );
+
+		light.position = pos;
+		light.castShadow = true;
+		light.shadowMapWidth = 2048;
+		light.shadowMapHeight = 2048;
+		var d = 100;
+		light.shadowCameraLeft = -d;
+		light.shadowCameraRight = d;
+		light.shadowCameraTop = d * 2;
+		light.shadowCameraBottom = -d * 2;
+
+		light.shadowCameraNear = 100;
+		light.shadowCameraFar = 600;
+		light.shadowCameraVisible = true;
 		VA3C.scene.add( light );
 
 // axes
@@ -46,9 +70,11 @@
 
 // ground box
 		geometry = new THREE.BoxGeometry( 200, 1, 100 );
-		material = new THREE.MeshNormalMaterial();
+		material = new THREE.MeshBasicMaterial( { color: 0xaaaaaa } );
 		mesh = new THREE.Mesh( geometry, material );
 		mesh.position.set( 0, -10, 0 );
+		mesh.castShadow = true;
+		mesh.receiveShadow = true;
 		VA3C.scene.add( mesh );
 
 		loadJS( VA3C.fname );
@@ -59,9 +85,19 @@
 		obj = new THREE.Object3D();
 		var loader = new THREE.ObjectLoader();
         loader.load(fname, function(obj){
+			obj.scale.set( 5, 5, 5 );
+			obj.castShadow = true;
+			obj.receiveShadow = true;
             VA3C.scene.add(obj);
         });
 		VA3C.scene.add( obj );
+	}
+
+	function v( x, y, z ){ return new THREE.Vector3( x, y, z ); }
+
+	function convertPosition( lat, lon, radius ) {
+		var rc = radius * Math.cos( lat * d2r );
+		return v( rc * Math.cos( lon * d2r ), radius * Math.sin( lat * d2r ), rc * Math.sin( lon * d2r) );
 	}
 
 	function animate() {
