3812237d39d2a51d29ea5d728509c84de8e5b694
Ben Howes <bhowes@thorntontomasetti.com>
2015-03-23 17:34:13 -0400

    added examples 2 and 3
    demonstrated how to include multiple viewers in one page (ex 2), and how
    to add some UI chunks to an embedded viewer (ex 3).

---
+++ examples/embeddedViewer002/js/APP_INIT.js
@@ -0,0 +1,34 @@
+/**
+ * Created by benjamin howes on 12/30/2014.
+ */
+
+//contains the jquery document.ready callback, which starts the application
+
+$(document).ready(function(){
+
+
+    //load JSON file 1
+    $.getJSON("./js/va3c.json", function( data ){
+
+        //once loaded, initialize a VA3C viewer by passing in the div to bind to, the json data, and a callback function
+        //where we can enable application functionality in nice clean chunks
+        var myVA3C = new VA3C_CONSTRUCTOR($("#vA3C_output1"), data, function(app){
+
+            //call the UI / functionality modules
+            app.setBackgroundColor(0xFFFFFF);
+        });
+    });
+
+    //load JSON file 2
+    $.getJSON("./js/va3c2.json", function( data ){
+
+        //once loaded, initialize a VA3C viewer by passing in the div to bind to, the json data, and a callback function
+        //where we can enable application functionality in nice clean chunks
+        var myVA3C = new VA3C_CONSTRUCTOR($("#vA3C_output2"), data, function(app){
+
+            //call the UI / functionality modules
+            app.setBackgroundColor(0xFFFFFF);
+        });
+    });
+
+});
---
+++ examples/embeddedViewer003/js/APP_INIT.js
@@ -0,0 +1,25 @@
+/**
+ * Created by benjamin howes on 12/30/2014.
+ */
+
+//contains the jquery document.ready callback, which starts the application
+
+$(document).ready(function(){
+
+
+    //load our sample JSON file - for development of the colored meshes from GH
+    $.getJSON("./js/va3c.json", function( data ){
+
+        //once loaded, initialize a VA3C viewer by passing in the div to bind to, the json data, and a callback function
+        //where we can enable application functionality in nice clean chunks
+        myVA3C = new VA3C_CONSTRUCTOR($("#vA3C_output"), data, function(app){
+
+            //call the UI / functionality modules
+            app.setBackgroundColor(0xFFFFFF);
+            app.userInterface();
+            app.sceneUI();
+            app.lightingUI();
+        });
+    });
+
+});
