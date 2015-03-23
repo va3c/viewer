2479fad87ccafdedb8dd35014dc4040ef6b6a85d
Ben Howes <bhowes@thorntontomasetti.com>
2015-03-23 17:41:03 -0400

    added example 4 - layers

---
+++ examples/embeddedViewer004/js/APP_INIT.js
@@ -0,0 +1,24 @@
+/**
+ * Created by benjamin howes on 12/30/2014.
+ */
+
+//contains the jquery document.ready callback, which starts the application
+
+$(document).ready(function(){
+
+
+    //load our sample JSON file
+    $.getJSON("./js/va3c2.json", function( data ){
+
+        //once loaded, initialize a VA3C viewer by passing in the div to bind to, the json data, and a callback function
+        //where we can enable application functionality in nice clean chunks
+        var myVA3C = new VA3C_CONSTRUCTOR($("#vA3C_output"), data, function(app){
+
+            //call the UI / functionality modules
+            app.setBackgroundColor(0xFFFFFF);
+            app.userInterface();
+            app.layersUI();
+        });
+    });
+
+});
